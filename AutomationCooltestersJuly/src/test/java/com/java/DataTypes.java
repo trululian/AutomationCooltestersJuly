67c34f6b025c1cd064a4942f819f6633d3e74ac2
Trulu pc <Trulu pc@192.168.1.21>
2021-07-24 11:02:59 -0500

    test

---
+++ AutomationCooltestersJuly/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte abc = 127;
+		short var = 30000;
+		long var2 = 2147483647;
+		int altura = (int)2.54;
+		System.out.println(altura);
+		
+	}
+
+}
