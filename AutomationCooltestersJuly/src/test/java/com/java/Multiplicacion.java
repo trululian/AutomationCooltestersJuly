9ecaf22d4946ea6567a0fd820e0689c255d16da2
Trulu pc <Trulu pc@192.168.1.21>
2021-07-24 13:42:55 -0500

    ejercicio 1 multiplicacion 3 enteros

---
+++ AutomationCooltestersJuly/src/test/java/com/java/Multiplicacion.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numA = 2, numB = 3 ,numC = 4;
+		int multiplicacion = numA*numB*numC;
+		System.out.println("la multiplicación de tres numeros enteros es: "+ multiplicacion);
+		
+	}
+
+}
